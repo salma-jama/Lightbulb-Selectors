6acd6c2b82f90bcafecab56bd09ec17446bd6f1e
DevSalma <devsalma6@gmail.com>
2023-04-20 13:08:32 +0100

    Added script and event listener

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Write your code here
+const bulbsList = document.querySelectorAll(".lightbulb");
+const subtitle = document.querySelector(".subtitle");
+let count = 0;
+
+function clickHandler(e) {
+    count++;
+    if(count === 1){
+        return subtitle.innerHTML = `You've clicked the lightbulbs ${count} time`;
+    };
+    subtitle.innerHTML = `You've clicked the lightbulbs ${count} times`;
+    e.currentTarget.classList.toggle("active");
+};
+
+bulbsList.forEach(function(bulb) {
+    bulb.addEventListener('click', clickHandler);
+});
